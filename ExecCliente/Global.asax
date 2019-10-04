e24f4a4d03f720e2e08ceace453102e01561f8a4
Tiagomunerato <tiagomunerato@gmail.com>
2019-10-04 14:03:13 -0300

    Add project files.

---
+++ ExecCliente/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExecCliente.MvcApplication" Language="C#" %>
---
+++ ExecCliente/Models/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExecCliente.Models
+{
+    public class Cliente
+    {
+
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public string Rg { get; set; }
+        public string Cpf { get; set; }
+        public string Datanascimento { get; set; }
+        public string Sexo { get; set; }
+        public string Estadocivil { get; set; }
+        public string Rendamensal { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+
+
+    }
+}
